6951e5d3c76d3e101d8e79adefb36f949a81ead6
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2025-04-15 09:12:22 +0200

    Refactor Post Layout and Implement Like Count - #31
    - Added content descriptions to the post image and like button for accessibility.
    - Added a TextView to display the number of likes for each post.
    - Updated the post layout to display the timestamp and username in a more visually appealing way.
    - Changed TextView color to improve readability.
    - Updated the `PostAdapter` to dynamically count and display the number of likes for each post from Firestore.
    - Implemented server-side count for like aggregation.
    - Added error handling for like count retrieval.

--- app/src/main/java/com/fakebook/SocialMediaApp/adapters/PostAdapter.kt
+++ app/src/main/java/com/fakebook/SocialMediaApp/adapters/PostAdapter.kt
@@ -1,5 +1,6 @@
 package com.fakebook.SocialMediaApp.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.fakebook.SocialMediaApp.DataModels.Post
 import com.fakebook.SocialMediaApp.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.AggregateSource
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
@@ -29,6 +31,7 @@ class PostAdapter(private var posts: List<Post>) :
         val tvTimestamp: TextView = itemView.findViewById(R.id.tvTimestamp)
         val tvUsername: TextView = itemView.findViewById(R.id.tvUsername)
         val btnLike: ImageButton = itemView.findViewById(R.id.btnLike)
+        val tvNumLikes: TextView = itemView.findViewById(R.id.tvNumLikes)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
@@ -54,6 +57,23 @@ class PostAdapter(private var posts: List<Post>) :
         holder.tvTimestamp.text = sdf.format(date)
 
         holder.tvUsername.text = post.username
+
+        // Count the number of likes for this post
+        firestore.collection("posts")
+            .document(post.postId)
+            .collection("likes")
+            .count()
+            .get(AggregateSource.SERVER)
+
+            .addOnSuccessListener { aggregateSnapshot ->
+                val numLikes = aggregateSnapshot.count
+                holder.tvNumLikes.text = numLikes.toString()
+            }
+            .addOnFailureListener { exception ->
+                Log.e("FirestoreError", "Error counting likes: ", exception)
+                holder.tvNumLikes.text = "0"
+            }
+
 
         // Reference to the like document for the current user in this post's like sub-collection
         val likeRef = firestore.collection("posts")
